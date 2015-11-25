482b2ab8a6be111ff8bbcffecb82ccb2fa584498
Masamoto Miyata <miyata@sincere-co.com>
2015-11-25 12:11:25 +0900

    値が配列になるパターンを実装。

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -6,9 +6,19 @@ class BuilderTest extends PHPUnit_Framework_TestCase
   public function testSimple()
   {
     $builder = new Mqb_Builder();
-    $query = $builder->query();
-    $query->add('some_column', 'value');
 
-    $this->assertEquals(array('some_column' => 'value'), $query->build());
+    $query = $builder->query();
+    $query->add('some_column', 'value1');
+    $this->assertEquals(array(
+      'some_column' => 'value1'
+    ), $query->build());
+
+    $query->add('some_column', 'value2');
+    $this->assertEquals(array(
+      'some_column' => array(
+        'value1',
+        'value2'
+      )
+    ), $query->build());
   }
 }
